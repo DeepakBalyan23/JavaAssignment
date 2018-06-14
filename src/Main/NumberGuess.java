199c00664248d9500a195189234fc5c3106fec1b
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-06-14 20:46:17 +0530

    ques_8

---
+++ src/Main/NumberGuess.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class NumberGuess {
+	
+	public String predictionResult(int num) {
+		final int x = 40;
+		if(num>x)
+			return "Number guessed is more than original number";
+		else if(num<x)
+			return "Number guessed is less than original number";
+		else
+			return "Number guessed matches the original number";
+	}
+}
---
+++ src/Main/NumberGuessTest.java
@@ -0,0 +1,40 @@
+package Main;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberGuessTest {
+	
+	@Test
+	public void testPredictionResult1() throws Exception {
+		//Arrange
+		String expectedValue = "Number guessed is more than original number";
+		NumberGuess pl=new NumberGuess();
+		//Act
+		String actualValue = pl.predictionResult(90);
+		//Assert
+		assertEquals(expectedValue, actualValue);
+	}
+	
+	@Test
+	public void testPredictionResult2() throws Exception {
+		//Arrange
+		String expectedValue = "Number guessed is less than original number";
+		NumberGuess pl=new NumberGuess();
+		//Act
+		String actualValue = pl.predictionResult(10);
+		//Assert
+		assertEquals(expectedValue, actualValue);
+	}
+	
+	@Test
+	public void testPredictionResult3() throws Exception {
+		//Arrange
+		String expectedValue = "Number guessed matches the original number";
+		NumberGuess pl=new NumberGuess();
+		//Act
+		String actualValue = pl.predictionResult(40);
+		//Assert
+		assertEquals(expectedValue, actualValue);
+	}
+}
