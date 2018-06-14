ec9c1e2a2d1e7087b0a303667d1832d23c41cb96
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-06-14 17:44:36 +0530

    ques_6

---
+++ src/Main/InputType.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class InputType {
+	
+	public String checkInputType(char ch) {
+		String str = Character.toString(ch); 
+		if(str.matches("[A-Z]+")) {
+			return "Capital Letter";
+		} else if(str.matches("[a-z]+")) {
+			return "Small Letter";
+		} else if(str.matches("[0-9]+")) {
+			return "Digit";
+		} else {
+			return "Special Symbol";
+		}
+	}
+}
